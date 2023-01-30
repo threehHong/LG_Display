a36a7ad508be2b28ee52dcaf0fdf1da03e1ecacb
jinac102 <monet158@naver.com>
2023-01-31 01:12:43 +0900

    230131_0112
    수정

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 // header_JA_230130 
-let gnb = $('.gnb_box > li');
-            let lnbBG = $('.wrapper');
-            let lnb = $('#header .lnb_box');
 
-            gnb.click(function(){
-                lnbBG.addClass('active');
-                lnb.addClass('active');
-            });
+let gnb = $('.gnb_box > li'),
+lnbBG = $('.wrapper'),
+lnb = $('#header .lnb_box');
+
+    $('.gnb_box').mouseenter(function(){
+        $('header').addClass('h_color'); //addremove로 수정
+    });
+    gnb.click(function(){
+        $(this).parent('ul').toggleClass('clicked');
+        lnbBG.toggleClass('active');
+        lnb.addClass('active');
+    });
 
 
 // /header_JA_230130 
