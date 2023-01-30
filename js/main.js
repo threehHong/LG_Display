61f1c9d4071012fa16d042952691937c2d7f0c2f
jinac102 <monet158@naver.com>
2023-01-31 00:10:59 +0900

    230130
    230130 작업본 업로드

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// header_JA_230130 
+let gnb = $('.gnb_box > li');
+            let lnbBG = $('.wrapper');
+            let lnb = $('#header .lnb_box');
+
+            gnb.click(function(){
+                lnbBG.addClass('active');
+                lnb.addClass('active');
+            });
+
+
+// /header_JA_230130 
