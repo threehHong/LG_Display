10078228141d01e14be249e18739465d934fc33d
Yunjung3 <pyunjung313@gmail.com>
2023-02-02 18:20:56 +0900

    230202
    230202 작업본

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 // header_JA_230130
 
 let gnb = $(".gnb_box"),
-    gnbList = $(".gnb_box > li"),
-    lnbBG = $(".wrapper"),
-    lnb = $(".lnb_box ");
+  gnbList = $(".gnb_box > li"),
+  lnbBG = $(".wrapper"),
+  lnb = $(".lnb_box ");
 
 gnbList.click(function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    let $this = $(this);
-    lnbBG.toggleClass("active");
-    gnb.toggleClass("open");
+  let $this = $(this);
+  lnbBG.addClass("active");
+  gnb.addClass("open");
+  $("header, .wrapper").mouseleave(function () {
+    lnbBG.removeClass("active");
+    gnb.removeClass("open");
+  });
 
-    $this.addClass("clicked");
-    $this.siblings().removeClass("clicked");
-    //추가 _ 다른 gnb 클릭 시(혹은 wrapper에 )
+  $this.addClass("clicked");
+  $this.siblings().removeClass("clicked");
 });
 
 // /header_JA_230130
