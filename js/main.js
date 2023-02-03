df754d3d8bbb887f847ecc62b33af60747941eb0
LeeWinJun <109531669+threehHong@users.noreply.github.com>
2023-02-03 17:03:51 +0900

    230203_17:03
    230203_17:03 작업본

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 // header_JA_230130
 
 let gnb = $(".gnb_box"),
-  gnbList = $(".gnb_box > li"),
-  lnbBG = $(".wrapper"),
-  lnb = $(".lnb_box ");
+    gnbList = $(".gnb_box > li"),
+    lnbBG = $(".wrapper"),
+    lnb = $(".lnb_box ");
 
 gnbList.click(function (e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  let $this = $(this);
-  lnbBG.addClass("active");
-  gnb.addClass("open");
-  $("header, .wrapper").mouseleave(function () {
-    lnbBG.removeClass("active");
-    gnb.removeClass("open");
-  });
+    let $this = $(this);
+    lnbBG.addClass("active");
+    gnb.addClass("open");
+    $("header, .wrapper").mouseleave(function () {
+        lnbBG.removeClass("active");
+        gnb.removeClass("open");
+    });
 
-  $this.addClass("clicked");
-  $this.siblings().removeClass("clicked");
+    $this.addClass("clicked");
+    $this.siblings().removeClass("clicked");
 });
 
 // /header_JA_230130
