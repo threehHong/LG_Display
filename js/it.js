5d0bc1e1dc18a7265d2c6d0fd708f683983203c7
uauu89 <119560529+uauu89@users.noreply.github.com>
2023-02-05 04:05:40 +0900

    230205 it 작업본 01
    css 반응형 포함 윤곽 작성
    스크립트 일부 작성

--- js/it.js
+++ js/it.js
@@ -1,21 +1,48 @@
 let section2 = $("#section2"),
-    section3 = $("#section3");
+    section3 = $("#section3"),
+    figSunlight = $("#section1 .sec_sunlight .notice"),
+    figBlack = $("#section1 .sec_black .notice"),
+    figBlackFrame = figBlack.find("div");
 
-let screenHeight = $(window).innerHeight(),
-    section2_Position = section2.offset().top,
-    section2_Height = section2.innerHeight(),
-    section3_Position = section3.offset().top;
+// let screenHeight = $(window).innerHeight(),
+//     section2_Position = section2.offset().top,
+//     section2_Height = section2.innerHeight(),
+//     section3_Position = section3.offset().top,
+//     figSunlight_Position = figSunlight.offset().top;
+    
+let screenHeight,
+    section2_Position,
+    section2_Height,
+    section3_Position,
+    figSunlight_Position;
     
 
-// console.log(screenHeight)
-// console.log(section2.offset().top);
+
+function resizeEvent(){
+    console.log("resize event");
+    screenHeight = $(window).innerHeight(),
+    section2_Position = section2.offset().top,
+    section2_Height = section2.innerHeight(),
+    section3_Position = section3.offset().top,
+    figSunlight_Position = figSunlight.offset().top;
+}
+
+$(window).resize(function(){
+    resizeEvent();
+});
+
+
 
 $(window).scroll(function(){
     let windowScroll = $(window).scrollTop();
-    // console.log(section3_Position - screenHeight /2);
-    // console.log("windowscroll", windowScroll);
+
+    if(windowScroll > figSunlight_Position - screenHeight / 2){
+        figSunlight.addClass("focus");
+    }else{
+        figSunlight.removeClass("focus");
+    }
     
-    if(windowScroll > section3_Position - screenHeight /2){
+    if(windowScroll > section3_Position + screenHeight /2){
         section2.add(section3).removeClass("focus");
     }else if(windowScroll > section2_Position - screenHeight/2){
         section2.add(section3).addClass("focus");
@@ -26,3 +53,18 @@ $(window).scroll(function(){
 
     //  || windowScroll < section2_Position - screenHeight/2 
 });
+
+figBlack.mousemove(function(){
+    let figPosX = figBlack.offset().left,
+        figWidth = figBlack.innerWidth(),
+        cursorX = event.pageX - figPosX;
+        // console.log("figPosX", figPosX)
+        // console.log("figWidth", figWidth);
+        console.log(cursorX / figWidth * 100);
+        figBlackFrame.css({width: cursorX / figWidth * 100 + "%"});
+})
+
+$(window).trigger("resize");
+console.log(figSunlight_Position);
+// sunlight event
+
