73006c85a166d0ddc1a0112511e3259052bb77e2
jinac102 <monet158@naver.com>
2023-02-09 02:18:02 +0900

    230208
    oled, media 추가

--- js/media.js
+++ js/media.js
@@ -1,25 +1,63 @@
+/* slide */
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    // loop: true,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    centeredSlides: true,
+    // Responsive breakpoints
+    breakpoints: {
+        390: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+        // when window width is >= 640px
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+$(".media_sec01_p .media_sec01_txt a").html(
+    $(swiper.slides[0]).find("h4").clone()
+);
+swiper.on("slideChange", function () {
+    let content = $(swiper.slides[swiper.activeIndex]).find("h4").clone();
+
+    console.log(content);
+    // swiper.slides[swiper.activeIndex.find("h4").clone()].appendTo("a");
+    $(".media_sec01_p .media_sec01_txt a").html(content);
+    $(".change_num").text(swiper.activeIndex + 1);
+});
+
 /* load more */
 let container = $("#gallery"),
-	loadMoreBtn = $("#load_more"),
-	addItemCount = 6, //표시할 개수
-	added = 0, //표시된 개수
-	allData = []; //json 파일내 내용을 담을 배열
+    loadMoreBtn = $("#load_more"),
+    addItemCount = 6, //표시할 개수
+    added = 0, //표시된 개수
+    allData = []; //json 파일내 내용을 담을 배열
 
 $.getJSON("./data/content.json", initGallery);
 
 function initGallery(result) {
-	allData = result;
-	console.log(allData);
-	addItems(); //목록 추가
-	loadMoreBtn.on("click", addItems);
+    allData = result;
+    console.log(allData);
+    addItems(); //목록 추가
+    loadMoreBtn.on("click", addItems);
 } //initGallery
 
 function addItems() {
-	let itemHTML = "",
-		slicedData = allData.slice(added, added + addItemCount);
+    let itemHTML = "",
+        slicedData = allData.slice(added, added + addItemCount);
 
-	$.each(slicedData, function (i, item) {
-		itemHTML += `
+    $.each(slicedData, function (i, item) {
+        itemHTML += `
         <li class="${item.data_category} g-5 col-xl-4 col-md-6">
           <a href="#">
             <img src="${item.images}" />
@@ -32,34 +70,34 @@ function addItems() {
           </a>
         </li>
       `;
-	});
-	container.append(itemHTML);
-	added += addItemCount; // added += slicedData
-	if (added < allData.length) {
-		loadMoreBtn.show();
-	} else {
-		loadMoreBtn.hide();
-	}
+    });
+    container.append(itemHTML);
+    added += addItemCount; // added += slicedData
+    if (added < allData.length) {
+        loadMoreBtn.show();
+    } else {
+        loadMoreBtn.hide();
+    }
 } //addItems
 
 /* tab */
 $(".tab_btn button").click(function (e) {
-	e.preventDefault();
-	// $(this).parent().find("button").removeClass("active");
-	$(this).toggleClass("active");
-	$(this).siblings().removeClass("active");
+    e.preventDefault();
+    // $(this).parent().find("button").removeClass("active");
+    $(this).toggleClass("active");
+    $(this).siblings().removeClass("active");
 
-	let targetValue = "";
-	let activeBtn = $(".tab_btn button.active");
+    let targetValue = "";
+    let activeBtn = $(".tab_btn button.active");
 
-	if ($(this).attr("data-filter") === "*") {
-		targetValue = "*";
-		$(".tab_btn button").removeClass("active");
-		$(".news_article li").show();
-		$(this).addClass("active");
-	} else {
-		targetValue = $(this).attr("data-filter");
-		$(".news_article li").hide();
-		$(".news_article li").filter(targetValue).show();
-	}
+    if ($(this).attr("data-filter") === "*") {
+        targetValue = "*";
+        $(".tab_btn button").removeClass("active");
+        $(".news_article li").show();
+        $(this).addClass("active");
+    } else {
+        targetValue = $(this).attr("data-filter");
+        $(".news_article li").hide();
+        $(".news_article li").filter(targetValue).show();
+    }
 });
--- js/oled.js
+++ js/oled.js
@@ -1,3 +1,144 @@
+/********** OLED EX(SECTION1) **********/
+/* $(window).resize(function(){
+    let h2_bottom_val = ( ($(window).height() / $(window).width()) * 45 );
+    console.log(h2_bottom_val);
+
+    $('.banner h2').css({ 
+        bottom: `${h2_bottom_val}%`,
+    })
+}) */
+
+let ex_intro = $('.EX_intro'),
+    oled = ex_intro.find('h1:nth-child(1)'),
+    ex = ex_intro.find('h1:nth-child(2)');
+    
+
+let count = 0,
+    count_al = 0,
+    count_bezel = 0;
+$(window).scroll(function(){
+    let scroll_AMT = $(window).scrollTop();
+    console.log('scroll_ATM :', scroll_AMT);
+
+    /* 꽃 피는 이미지 이벤트 */
+    let banner_height = $(".banner_container").innerHeight();
+
+    count = Math.floor(((scroll_AMT) / banner_height) * 24)
+    if(count > 24) { count = 24 }
+    // $(".banner").css({"background-image":`url(../img/flower/flower${count}.jpg)`,})
+    $(".banner img").attr({"src":`/img/flower/flower${count}.jpg`})
+    /* 꽃 피는 이미지 이벤트 */
+
+    // OLED EX 문구(좌우 등장)
+    let oled_value = -5000 + scroll_AMT; 
+    let ex_value = 5000 - scroll_AMT;
+
+    if(oled_value < 0) {
+        oled.css({
+            transform: `translateX(${oled_value}px)`
+        })    
+        ex.css({
+            transform: `translateX(${ex_value}px)`
+        })    
+    } else if(oled_value  > -500) {  
+        oled.addClass('active_intro_stop');
+        ex.addClass('active_intro_stop') 
+    }
+
+    if(scroll_AMT < 4800 && scroll_AMT > 4700) {
+        oled.removeClass('active_intro_stop');
+        ex.removeClass('active_intro_stop');
+    }
+    // console.log('olled_value', oled_value);
+
+
+
+    // EX INTRO P태그 fade-in 등장
+    if(scroll_AMT > 3200 ) {
+        $('.EX_intro p').addClass('active_EX_intro');
+        // $('.EX_intro p').fadeIn();
+    } else {
+        $('.EX_intro p').removeClass('active_EX_intro');
+    } 
+    
+
+
+    // EX INTRO에서 activ_EX_stick 클래스 제거
+    if(scroll_AMT > 13000) { /* 13200 */
+        ex_intro.css({ transform: 'translateY(8000px)' });
+        ex_intro.removeClass('active_EX_sticky');
+    } else if (scroll_AMT <= 12999 && scroll_AMT >= 12000) {
+        ex_intro.css({ transform: 'translateY(0px)' });
+        ex_intro.addClass('active_EX_sticky');
+    } 
+    // console.log(ex_intro.offset().top)
+
+
+
+    // 중수소 소자 스크롤 이벤트 
+    if(scroll_AMT >= 16200 && scroll_AMT <= 19000) {
+        $('.EX_deuterium h2').addClass('active_deuterium_h2');
+        $('.EX_deuterium p').addClass('active_deuterium_p')
+    } else if(scroll_AMT > 20000) {
+        $('.EX_deuterium h2').removeClass('active_deuterium_h2');
+        $('.EX_deuterium p').removeClass('active_deuterium_p')
+    } else {
+        $('.EX_deuterium h2').removeClass('active_deuterium_h2');
+        $('.EX_deuterium p').removeClass('active_deuterium_p')
+    } 
+    
+
+
+    // 알고리즘 이미지 시퀀스 이벤트
+    let algorism_offsetTop = $('.EX_algorism_container').offset().top,
+        algorism_innerHieght = $('.EX_algorism_container').innerHeight();
+    if(scroll_AMT > 21700) {
+        count_al = Math.floor(((scroll_AMT - algorism_offsetTop) / algorism_innerHieght) * 150);
+        
+        if(count_al > 96) { count_al = 96 } /* 98 */
+        console.log(count_al);
+
+        $(".EX_algorism img").attr({"src":`../img/algorism/dotted${count_al}.jpg`})
+    } 
+    // 알고리즘 이미지 스크롤 이벤트
+    if(scroll_AMT >= 31000) {
+        $('.EX_algorism .img').addClass('active_algorism_opacity');
+        $('.EX_algorism img').addClass('active_algorism_opacityS')
+    } else {
+        $('.EX_algorism .img').removeClass('active_algorism_opacity');
+        $('.EX_algorism img').removeClass('active_algorism_opacityS')
+    }
+
+
+
+    // 베젤 이미지 시퀀스 이벤트
+    let bezel_offsetTop = $('.EX_bezel_container').offset().top,
+        bezel_innerHieght = $('.EX_bezel_container').innerHeight();
+    if(scroll_AMT > 34800) {
+        count_bezel = Math.floor(((scroll_AMT - bezel_offsetTop) / bezel_innerHieght) * 10);
+        console.log(count_bezel);
+        if(count_bezel > 9) { count_bezel = 9 }
+       
+        $(".EX_bezel img").attr({"src":`../img/bezel/bezel${count_bezel}.jpg`})
+    }
+
+})
+
+/* SKROLLR */
+var s = skrollr.init();
+
+/* 모바일 문제 */
+/* $(window).resize(function() {
+    if($(window).innerWidth() < 480) {
+        s.destroy();
+    }
+}) */
+
+/********** OLED EX(SECTION1) **********/
+
+
+
+/********** OLED ACE(SECTION2) **********/
 AOS.init();
 
 let se02_$window = $(window),
@@ -18,6 +159,13 @@ se02_$window.scroll(function () {
         se02_$ACE_clone_inner.removeClass("visible");
     }
 });
+$('header').mouseover(function(){
+    $('.ace_clone.visible').addClass('headerHeight');
+});
+se02_$window.scroll(function(){
+    $('.ace_clone.visible').removeClass('headerHeight');
+});
+//NAV_toggleclass 추가 시 sticky_ace 높이 수정
 
 //ACE_A1_img
 let aceA1Position = $(".ACE_A").offset().top,
@@ -48,3 +196,18 @@ $(window).scroll(function () {
     } else {
     }
 });
+//반응형
+// se02_$window.resize(function(){ 
+//     if (se02_$window.innerWidth > 480) {  // 다바이스 크기가 480이상일때 
+    
+//     /* 스크립트내용*/ 
+    
+//     } else {
+    
+//     /* 스크립트내용*/ 
+    
+//     }
+    
+//     }).resize(); 
+
+/********** OLED ACE(SECTION2) **********/
